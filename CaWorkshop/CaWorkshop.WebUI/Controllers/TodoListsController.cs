6b102485b3b21b7989c1c22d4ad8e3e042307406
Peter St Angelo <psa@outlook.com>
2020-07-09 04:48:38 +0200

    move models to domain project and update references

--- CaWorkshop/CaWorkshop.WebUI/Controllers/TodoListsController.cs
+++ CaWorkshop/CaWorkshop.WebUI/Controllers/TodoListsController.cs
@@ -6,7 +6,7 @@ using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using CaWorkshop.WebUI.Data;
-using CaWorkshop.WebUI.Models;
+using CaWorkshop.Domain.Entities;
 
 namespace CaWorkshop.WebUI.Controllers
 {
