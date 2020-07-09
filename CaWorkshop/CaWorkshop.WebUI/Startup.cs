7b4ce521a542afbc3fc884603a2bc4af4e17feb9
Peter St Angelo <psa@outlook.com>
2020-07-09 05:28:20 +0200

    clean up dependency injection location and references

--- CaWorkshop/CaWorkshop.WebUI/Startup.cs
+++ CaWorkshop/CaWorkshop.WebUI/Startup.cs
@@ -1,3 +1,5 @@
+using CaWorkshop.Application;
+using CaWorkshop.Infrastructure;
 using CaWorkshop.Infrastructure.Identity;
 using CaWorkshop.Infrastructure.Persistence;
 using Microsoft.AspNetCore.Authentication;
@@ -26,18 +28,9 @@ namespace CaWorkshop.WebUI
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddDbContext<ApplicationDbContext>(options =>
-                options.UseSqlServer(
-                    Configuration.GetConnectionString("DefaultConnection")));
+            services.AddInfrastructure(Configuration);
+            services.AddApplication(Configuration);
 
-            services.AddDefaultIdentity<ApplicationUser>(options => options.SignIn.RequireConfirmedAccount = true)
-                .AddEntityFrameworkStores<ApplicationDbContext>();
-
-            services.AddIdentityServer()
-                .AddApiAuthorization<ApplicationUser, ApplicationDbContext>();
-
-            services.AddAuthentication()
-                .AddIdentityServerJwt();
             services.AddControllersWithViews();
             services.AddRazorPages();
             // In production, the Angular files will be served from this directory
